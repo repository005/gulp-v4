4352893f62f72cfa7d4de54562bd944d7a5711fb
Avem <rumbayhandsup@mail.ru>
2018-10-31 15:43:27 +0300

    gulpv4 beta 1.5

--- gulpfile.js
+++ gulpfile.js
@@ -72,9 +72,9 @@ gulp.task('sass', function() {
 
 gulp.task('scripts:libs', function() {
   return gulp.src(
-      ['node_modules/jquery/dist/jquery.min.js'],
-      ['node_modules/slick-carousel/slick/slick.min.js'],
-      ['node_modules/svg4everybody/dist/svg4everybody.min.js']
+      ['node_modules/jquery/dist/jquery.min.js',
+      'node_modules/slick-carousel/slick/slick.min.js',
+      'node_modules/svg4everybody/dist/svg4everybody.min.js']
     )
     .pipe(glp.concat('libs.min.js'))
     .pipe(gulp.dest('build/js/'))
